7595e924034269f7606a4ab74ad5332962861ac9
wahid-ari <wahiid.arii@gmail.com>
2023-09-12 16:37:29 +0700

    UPDATE Author total-book-quote API

--- pages/api/author/total-book-quote.ts
+++ pages/api/author/total-book-quote.ts
@@ -7,7 +7,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   switch (method) {
     case 'GET':
-      const { data } = await supabase.from('book_authors').select(`*, book_books (id), book_quotes (id)`).order('id');
+      const { data } = await supabase
+        .from('book_authors')
+        .select(`id,name, image, slug, book_books (id), book_quotes (id)`)
+        .order('id');
       // TODO Docs https://nextjs.org/docs/api-reference/next.config.js/headers#cache-control
       res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59');
       res.status(200).json(data);
