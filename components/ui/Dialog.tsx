137036870022c570cb6b262c3f424e891d8bd5ad
wahid-ari <wahiid.arii@gmail.com>
2023-11-03 09:06:06 +0700

    feat: upgrade radix-ui UI design page

--- components/ui/Dialog.tsx
+++ components/ui/Dialog.tsx
@@ -78,7 +78,7 @@ const DialogTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Title
     ref={ref}
-    className={cn('text-lg font-semibold leading-none tracking-tight', className)}
+    className={cn('text-lg font-semibold leading-none', className)}
     {...props}
   />
 ));
