c00a3469986b9e2de591b74f833487c192ede3dd
wahid-ari <wahiid.arii@gmail.com>
2023-11-24 19:48:52 +0700

    feat: update API endpoint, count total book that have same genre

--- pages/api/genre/total-book.ts
+++ pages/api/genre/total-book.ts
@@ -19,29 +19,29 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           total: 0,
         });
       }
+      // console.log(items)
+      // [
+      //   { id: 1, label: 'Art', slug: 'art', total: 0 },
+      //   { id: 2, label: 'Biography', slug: 'biography', total: 0 }
+      // ]
       // Count total book that have same genre
       let result = [];
       for (const item of items) {
-        for (const book_genre of books_genres) {
-          if (book_genre.genre_id == item.id) {
-            let filtered = items.filter((i) => i.id == book_genre.genre_id)[0];
-            filtered.total += 1;
-            result.push(filtered);
-          }
-        }
+        let filtered = books_genres.filter((i) => i.genre_id == item.id);
+        result.push({
+          ...item,
+          total: filtered.length,
+        });
       }
-      // Remove duplicate values from an array of objects in javascript
-      // TODO Docs https://stackoverflow.com/questions/45439961/remove-duplicate-values-from-an-array-of-objects-in-javascript
-      let data = result.reduce((unique, o) => {
-        if (!unique.some((obj: any) => obj.id === o.id)) {
-          unique.push(o);
-        }
-        return unique;
-      }, []);
-      data.sort((a: any, b: any) => b.total - a.total);
+      // console.log(result);
+      // [
+      //   { id: 1, label: 'Art', slug: 'art', total: 1 },
+      //   { id: 2, label: 'Biography', slug: 'biography', total: 9 }
+      // ]
+      result.sort((a: any, b: any) => b.total - a.total);
       // TODO Docs https://nextjs.org/docs/api-reference/next.config.js/headers#cache-control
       res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59');
-      res.status(200).json(data);
+      res.status(200).json(result);
       break;
 
     default:
