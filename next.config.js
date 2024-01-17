dbbe04527e401aadee648a488c8e33f061ec8350
wahid-ari <wahiid.arii@gmail.com>
2024-01-17 20:43:00 +0700

    feat: update next.config change from domains to remotePatterns

--- next.config.js
+++ next.config.js
@@ -23,7 +23,29 @@ module.exports = {
   },
   reactStrictMode: true,
   images: {
-    domains: ['images-na.ssl-images-amazon.com', 'i.gr-assets.com', 'images.unsplash.com'],
+    // https://nextjs.org/docs/pages/api-reference/components/image#domains
+    // domains: ['images-na.ssl-images-amazon.com', 'i.gr-assets.com', 'images.unsplash.com'],
+    // https://nextjs.org/docs/pages/api-reference/components/image#remotepatterns
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images-na.ssl-images-amazon.com',
+        port: '',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.gr-assets.com',
+        port: '',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
     unoptimized: true,
   },
 };
